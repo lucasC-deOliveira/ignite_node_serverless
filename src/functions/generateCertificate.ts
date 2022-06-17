1bf6d40966bfa618131b8c2dc15ef1496c465a39
Lucas Carvalho <67061874+lucasC-deOliveira@users.noreply.github.com>
2022-06-16 22:58:50 -0300

    Convertendo para pdf, com erro

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,34 +1,36 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
 
-import {document} from "../utils/dynamodbClient"
+import { document } from "../utils/dynamodbClient"
 
-import {compile} from "handlebars"
+import { compile } from "handlebars"
 
-import {join} from "node:path"
+import { join } from "node:path"
 
-import { readFileSync} from "node:fs"
+import { readFileSync } from "node:fs"
 
-import * as dayjs from "dayjs"
+import dayjs from "dayjs"
 
-interface ICreateCertificate{
-  id:string;
-  name:string;
-  grade:string;
+import chromium from "chrome-aws-lambda"
+
+interface ICreateCertificate {
+  id: string;
+  name: string;
+  grade: string;
 }
 
-interface ITemplate{
-  id:string;
-  name:string;
-  grade:string;
-  medal:string;
-  date:string;
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
 
 }
 
-const compileTemplate = async (data:ITemplate) => {
-  const filePath = join(process.cwd(),"src","templates","certificates.hbs")
+const compileTemplate = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), "src", "templates", "certificate.hbs")
 
-  const html = readFileSync(filePath,"utf-8")
+  const html = readFileSync(filePath, "utf-8")
 
   return compile(html)(data)
 }
@@ -37,8 +39,8 @@ export const handler: APIGatewayProxyHandler = async (event) => {
   const { id, name, grade } = JSON.parse(event.body)
 
   await document.put({
-    TableName:"users_certificate",
-    Item:{
+    TableName: "users_certificate",
+    Item: {
       id,
       name,
       grade,
@@ -48,17 +50,18 @@ export const handler: APIGatewayProxyHandler = async (event) => {
   }).promise()
 
   const response = await document.query({
-    TableName:"users_certificate",
-    KeyConditionExpression:"id = :id",
-    ExpressionAttributeValues:{
-      ":id":id
+    TableName: "users_certificate",
+    KeyConditionExpression: "id = :id",
+    ExpressionAttributeValues: {
+      ":id": id
     }
   }).promise()
-const medalPath = join(process.cwd(),"src","templates","selo.png")
 
-const medal = readFileSync(medalPath,"base64");
+  const medalPath = join(process.cwd(), "src", "templates", "selo.png")
 
-  const data ={
+  const medal = readFileSync(medalPath, "base64");
+
+  const data = {
     name,
     id,
     grade,
@@ -66,7 +69,27 @@ const medal = readFileSync(medalPath,"base64");
     medal
   }
 
-  const content =await compileTemplate(data)
+  const content = await compileTemplate(data)
+
+  const browser = await chromium.puppeteer.launch({
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    executablePath: await chromium.executablePath
+  })
+
+  const page = await browser.newPage();
+
+  await page.setContent(content)
+
+  await page.pdf({
+    format: "a4",
+    landscape: true,
+    printBackground: true,
+    preferCSSPageSize: true,
+    path: process.env.ID_OFFLINE ? "./certificate.pdf" : null
+  })
+  await browser.close();
+
 
   return {
     statusCode: 201,
